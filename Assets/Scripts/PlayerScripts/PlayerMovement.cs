ab5d8d191e582a5614ece58c90c4f62e5f90163f
amtaroo <boonyanee.th@gmail.com>
2024-08-01 14:27:49 +0700

    update  speedboost

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -38,4 +39,61 @@ public class PlayerMovement : MonoBehaviour
         }
     }
     
+}
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float normalSpeed = 5f;
+    public float boostedSpeed = 10f;
+    public float boostDuration = 5f;
+
+    private float currentSpeed;
+    private bool isBoosted = false;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        currentSpeed = normalSpeed;
+    }
+
+    private void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveX, moveY).normalized * currentSpeed;
+        rb.velocity = movement;
+
+        if (animator != null)
+        {
+            animator.SetFloat("Speed", movement.magnitude);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("SpeedBoost"))
+        {
+            StartCoroutine(SpeedBoost());
+            Destroy(collision.gameObject); // ลบไอเท็มออกเมื่อชน
+        }
+    }
+
+    private IEnumerator SpeedBoost()
+    {
+        isBoosted = true;
+        currentSpeed = boostedSpeed;
+
+        yield return new WaitForSeconds(boostDuration);
+
+        currentSpeed = normalSpeed;
+        isBoosted = false;
+    }
 }
